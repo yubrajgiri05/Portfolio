c8c26ce4a955d3fe117adf265433547a54609375
yubrajgiri05 <giriyubraj787898@gmail.com>
2023-04-19 00:13:18 +0545

    Updates

---
+++ precache-manifest.d025c3fa3e495563fd7c3581a3268ebf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7931b8358a4b84fb3d10",
+    "url": "/Portfolio/static/css/main.98773ed8.chunk.css"
+  },
+  {
+    "revision": "7931b8358a4b84fb3d10",
+    "url": "/Portfolio/static/js/main.7931b835.chunk.js"
+  },
+  {
+    "revision": "4b91df356e1331ffa28f",
+    "url": "/Portfolio/static/css/1.9bc3b532.chunk.css"
+  },
+  {
+    "revision": "4b91df356e1331ffa28f",
+    "url": "/Portfolio/static/js/1.4b91df35.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "f3668a1df5e7c0e7270d9b18d44f0870",
+    "url": "/Portfolio/static/media/home.f3668a1d.svg"
+  },
+  {
+    "revision": "1a567371032ffb1b69c5d719f79f5899",
+    "url": "/Portfolio/static/media/technical-support-137-1087325.1a567371.png"
+  },
+  {
+    "revision": "089c6f85929620dfab10728afb9fe828",
+    "url": "/Portfolio/static/media/web.089c6f85.png"
+  },
+  {
+    "revision": "c0a3c656af08ed90fb4fb45481075bcd",
+    "url": "/Portfolio/static/media/mad_blog_5df2190b8638a1576147211.c0a3c656.jpg"
+  },
+  {
+    "revision": "5b67f91f24bfdcfc9453ee57ee4af393",
+    "url": "/Portfolio/static/media/AndroidStudio-Intro-feature.5b67f91f.png"
+  },
+  {
+    "revision": "c5a2d6e272d729f2706a9d09b864aad3",
+    "url": "/Portfolio/static/media/digital-marketing.c5a2d6e2.png"
+  },
+  {
+    "revision": "52760becb07b61214e9e7eb0036a497f",
+    "url": "/Portfolio/static/media/marketing-plan_1.52760bec.jpg"
+  },
+  {
+    "revision": "86dcc26e54aeb4da1001a2b9a32839bb",
+    "url": "/Portfolio/static/media/Custom-Software-Development.86dcc26e.jpg"
+  },
+  {
+    "revision": "8ed34ac5d3822e751dde7336f5ef6500",
+    "url": "/Portfolio/index.html"
+  }
+];
